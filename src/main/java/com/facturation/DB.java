0eb260fe0c4ec30242babd50ae44dda9b500693d
JuMANZen <julien.mangeat@mail-formateur.net>
2022-06-07 09:44:56 +0200

    ajout params

--- src/main/java/com/facturation/DB.java
+++ src/main/java/com/facturation/DB.java
@@ -8,9 +8,13 @@ import javax.swing.JOptionPane;
 public class DB {
     Connection DBConnection(){
         Connection conn = null;
+        String url = "jdbc:mysql://localhost:3306/facturation";
+        String user = "root";
+        String password = "";
+
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/facturation", "root", "");
+            conn = DriverManager.getConnection(url, user, password);
             System.out.println("Connected to database");
         }catch(Exception e){
             JOptionPane.showMessageDialog(null, "Pas de connexion à la BDD " + e.getMessage(), "Erreur Connexion", JOptionPane.ERROR_MESSAGE);
